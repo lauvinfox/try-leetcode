8215a5175181e2426e4faced6d32a9798b11ea31
lauvinfox <miftahilmanakun@gmail.com>
2023-04-23 14:52:28 +0700

    finished

---
+++ addTwoNumber.py
@@ -0,0 +1,18 @@
+def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    result = ListNode()
+    pointer = result
+    carry = 0
+
+    while l1 or l2 or carry:
+        val = carry
+        if l1:
+            val += l1.val
+            l1 = l1.next
+        if l2:
+            val += l2.val
+            l2 = l2.next
+
+        carry, val = divmod(val, 10)
+        pointer.next = ListNode(val)
+        pointer = pointer.next
+    return result.next
